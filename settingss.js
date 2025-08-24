bac813019be0cfe08bf3c8a45050368c7137b414
yomin57744-cyber <yomin57744@gmail.com>
2025-08-24 13:18:33 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~hn9ExSgY#vC4B1mTkXg8n5f5mDu51_-USePqFBO44XN64h9PudUI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923154890611", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
